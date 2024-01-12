bcf21b1f9052a62e5a268ce4e4f3c1ab1543af32
Nyarlathotep <anhedonia@skiff.com>
2024-01-12 08:18:38 -0500

    feat: setup vitest github action

---
+++ src/App.spec.tsx
@@ -0,0 +1,21 @@
+import App from './App.tsx'
+import { render } from '@solidjs/testing-library'
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+
+describe('<App />', () => {
+  beforeEach(() => {
+    vi.mock('@/services/readFiles.ts', () => ({
+      default: async () => []
+    }))
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  test('render', async () => {
+    const { container, unmount } = render(() => <App />)
+    expect(container.firstElementChild?.tagName).toBe('DIV')
+    unmount()
+  })
+})
