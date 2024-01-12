568cf8c93882a7e0f72d27c00013ff6d9b8cb092
Nyarlathotep <anhedonia@skiff.com>
2024-01-12 06:30:17 -0500

    feat: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import TSConfigPath from 'vite-tsconfig-paths'
 import solid from 'vite-plugin-solid'
+import TSConfigPath from 'vite-tsconfig-paths'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   server: { port: 8080 },
-
+  test: { environment: 'jsdom' },
   plugins: [vanillaExtractPlugin(), TSConfigPath(), solid()]
 })
